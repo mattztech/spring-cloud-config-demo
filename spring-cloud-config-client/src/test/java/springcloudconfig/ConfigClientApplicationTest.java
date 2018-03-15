22fbc3e2431dcc0e2a14a6f423c2836dbee07e40
Matt Blackford <mzb@phoebussoftware.com>
2018-03-15 17:44:41 +0000

    Added client main class

---
+++ spring-cloud-config-client/src/test/java/springcloudconfig/ConfigClientApplicationTest.java
@@ -0,0 +1,15 @@
+package springcloudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigClientApplicationTest {
+
+  @Test
+  public void contextLoads() {}
+
+}
